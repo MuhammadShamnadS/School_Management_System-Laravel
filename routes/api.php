55be9e9715a4972b3d38d8e0346ac7b860be1642
MuhammadShamnadS <219637101+MuhammadShamnadS@users.noreply.github.com>
2025-08-06 10:07:46 +0530

    added routes

--- routes/api.php
+++ routes/api.php
@@ -9,23 +9,30 @@ use App\Http\Controllers\StudentController;
 
 //Authentication
 Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register'])->middleware(['auth:api', 'role:admin']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+
 
 
 //Admin CRUD (Teachers + Students)
 Route::middleware(['auth:api', 'role:admin'])->group(function () {
+
+    Route::post('/register/teacher', [AuthController::class, 'registerTeacher']);
+    Route::post('/register/student', [AuthController::class, 'registerStudent']);
     Route::apiResource('teachers', TeacherController::class);
     Route::apiResource('students', StudentController::class);
+    Route::get('/teachers/{teacherId}/students', [StudentController::class, 'studentsByTeacher']);
 });
 
 //Teacher Routes
 Route::middleware(['auth:api', 'role:teacher'])->group(function () {
-    Route::get('my-teacher', [TeacherController::class, 'myProfile']); 
-    Route::get('my-students', [StudentController::class, 'myStudents']); 
+    Route::get('/my-detail', [TeacherController::class, 'myProfile']); 
+    Route::get('/my-students', [StudentController::class, 'myStudents']); 
 });
 
 //Student Routes
-Route::middleware(['auth:api', 'role:student'])->get('my-student', [StudentController::class, 'myProfile']);
-Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
+Route::middleware(['auth:api', 'role:student'])->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::get('/my-details', [StudentController::class, 'myProfile']);
+});
 
