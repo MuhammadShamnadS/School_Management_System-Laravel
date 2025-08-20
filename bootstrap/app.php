ea7c3e3f2c5a1996bea2a8546d7e9088ca11ffb7
MuhammadShamnadS <219637101+MuhammadShamnadS@users.noreply.github.com>
2025-08-20 18:12:03 +0530

    fix- added middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'checkUserExists' => \App\Http\Middleware\CheckUserExists::class,
         ]);
            
     })
